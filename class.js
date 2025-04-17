92f158a501f1cd61aa42d04e81e5a8d0c42a18df
elProfessor077 <dwivediparth7777@gmail.com>
2025-04-18 00:10:24 +0530

    first commit

---
+++ class.js
@@ -0,0 +1,38 @@
+// // // // //classes and objects
+// // // // class ClassName{
+// // // //     constructor(prop1,prop2){
+// // // //         this.prop1=prop1;
+// // // //         this.prop2=prop2;
+
+// // // //     }
+// // // // }
+// // // // let obj= new ClassName("arg1","arg2");
+// // // // console.log(obj.prop1);
+// // // // console.log(obj.prop2);
+// // // // //this() keyword refres to the objects it belong to so it is the first property instance.
+// // // clas Dog{
+// // //     constructor(doganme,weight,color,breed){
+// // //         this.dogName=dogName;
+// // //         this.weight=weight;
+// // //         this.color=color;
+// // //         this.breed=breed;
+
+// // //     }
+// // // }
+// // // let dog=new Dog("Javascript",2.4,"brown","chihuahua") 
+// // // console.log(dog.dogName);
+// // //there is only one constructor in the class.
+// // //class name should be start with capital letter.
+// // ;
+// //     }
+// // }
+// // let p=new Person("Parth","Dwivedi");
+// // console.log("hi",p.firstName);
+// //METHODS:when defining these methods we dont use the function keywords,we directly start with new.
+// class Person{
+//         constructor(firstName,lastName){
+//             this.firstName=firstName;
+//             this.lastName=lastName;
+//         }
+//     }
+    
--- function.js
+++ function.js
@@ -1,36 +1,101 @@
-// // // // //returning with arrow function
-// // // // let addTwonumbers=(x,y)=>x+y;
-// // // // let  result=addTwonumbers(12,16);
-// // // // console.log(result);
-// // // // variable scope in functions
-// // // //you can use variable before declaration but not in let and constant
-// // // // function testAvailability(x){
+// // // // // // // // // // // // // //returning with arrow function
+// // // // // // // // // // // // // let addTwonumbers=(x,y)=>x+y;
+// // // // // // // // // // // // // let  result=addTwonumbers(12,16);
+// // // // // // // // // // // // // console.log(result);
+// // // // // // // // // // // // // variable scope in functions
+// // // // // // // // // // // // //you can use variable before declaration but not in let and constant
+// // // // // // // // // // // // // function testAvailability(x){
+// // // // // // // // // // // // // }
+// // // // // // // // // // // // // testAvailability("hi !");
+// // // // // // // // // // // // // console.log("not available here",x);
+// // // // // // // // // // // // // function testAvailability(){
+// // // // // // // // // // // // //     let y="local variable";
+// // // // // // // // // // // // //     console.log("available here",y);
+// // // // // // // // // // // // // }
+// // // // // // // // // // // // // testAvailability();
+// // // // // // // // // // // // // console.log("not available here",y);
+// // // // // // // // // // // // function doingstuff(){
+// // // // // // // // // // // //     if(true){
+// // // // // // // // // // // //         var x="local";
+// // // // // // // // // // // //     }
+// // // // // // // // // // // //     console.log(x)
+// // // // // // // // // // // // }
+// // // // // // // // // // // // doingstuff();
+// // // // // // // // // // // function doingstuff(){
+// // // // // // // // // // //     if(true){
+// // // // // // // // // // //         let x="local";
+// // // // // // // // // // //     }
+// // // // // // // // // // //     console.log(x)
+// // // // // // // // // // // }
+// // // // // // // // // // // doingstuff();
+// // // // // // // // // // // global function 
+// // // // // // // // // // let GlobalVar="accessible everywhere";
+// // // // // // // // // // console.log("outside the scope of function",GlobalVar);
+// // // // // // // // // // function GlobalVarScope(x){
+// // // // // // // // // //     console("inside the function"GlobalVar)
+// // // // // // // // // // }
+// // // // // // // // // //immediately invoked function expression
+// // // // // // // // // (function{
+// // // // // // // // //     console.log("IIFE");
+// // // // // // // // // })();
+// // // // // // // // (()=>{
+// // // // // // // //  console.log("run") 
+// // // // // // // // })();
+// // // // // // // //Recursive functions
+// // // // // // // // function getRecursive(nr){
+// // // // // // // //     console.log(nr);
+// // // // // // // //     getRecursive(--nr);
+// // // // // // // // }
+// // // // // // // // getRecursive(3);//infinite recusrsion
+// // // // // // // function getRecursive(nr){
+// // // // // // //     console.log(nr);
+// // // // // // //     if(nr>0){
+// // // // // // //     getRecursive(--nr);
+// // // // // // //     }
+// // // // // // // }
+// // // // // // // getRecursive(3);
+// // // // // // function logRecursive(nr){
+// // // // // //     console.log("started function",nr);
+// // // // // //     if(nr>0){
+// // // // // //         logRecursive(nr-1);
+// // // // // //     }else{
+// // // // // //         console.log("done with recursion");
+// // // // // //     }
+// // // // // //     console.log("ended function",nr)
+// // // // // // }
+// // // // // // logRecursive(3);
+// // // // // //the performance of recursion is worse as compared to iteration due to bottleneck and memory management.
+// // // // // //Nested Functions
+// // // // // function doOuterFunction(nr){
+// // // // //     console.log("outer function");
+// // // // //     doInnerFuction(x){
+// // // // //         console.log(x+7);
+// // // // //     }
+
+// // // // // }
+// // // // //Anonymous Functions
+// // // // //Function Callbacks
+// // // // let functionVariable=function(){
+// // // //     console.log("not so secret");
 // // // // }
-// // // // testAvailability("hi !");
-// // // // console.log("not available here",x);
-// // // // function testAvailability(){
-// // // //     let y="local variable";
-// // // //     console.log("available here",y);
+// // // // function doFlexibleStuff(executestuff){
+// // // //     executestuff();
+// // // //     console.log("inside doFlexibleStuff");
 // // // // }
-// // // // testAvailability();
-// // // // console.log("not available here",y);
-// // // function doingstuff(){
-// // //     if(true){
-// // //         var x="local";
-// // //     }
-// // //     console.log(x)
+// // // // doFlexibleStuff(functionVariable);
+// // // //setTimeout in milisecond
+// // // let youGotThis=function(){
+// // //     console.log("you are doing really well");
 // // // }
-// // // doingstuff();
-// // function doingstuff(){
-// //     if(true){
-// //         let x="local";
+// // // setTimeout(youGotThis,70000);
+// // //setInterval
+// // let youGotThis=function(){
+// //         console.log("you are doing really well");
 // //     }
-// //     console.log(x)
-// // }
-// // doingstuff();
-// // global function 
-// let GlobalVar="accessible everywhere";
-// console.log("outside the scope of function",GlobalVar);
-// function GlobalVarScope(x){
-//     console("inside the function"GlobalVar)
-// }
+// //  setInterval(youGotThis,70000);
+// (function(){
+//     console.log("welcome");
+// })();
+// let result=(function(){
+//     let firstname
+// })   
